2b1dc437b5d0de4cbe13e03a4d25627978699073
Boing <148807368+WillyHergeselDias@users.noreply.github.com>
2024-09-15 22:30:02 -0300

    Atualizar o ChatBotting.py

--- ChatBotting.py
+++ ChatBotting.py
@@ -4,6 +4,6 @@ x = input("Informe o que voce deseja falar: ")
 py.click(x = 760, y = 1050)
 time.sleep(5)
 py.click(x = 760, y = 900)
-for z in range(1):
+for z in range(10):
     py.write(x)
     py.press("enter")
